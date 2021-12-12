9282d7b83f48f42db730045bd43ade819a313bd5
Mark Fulton <mfulton26@gmail.com>
2021-12-11 20:52:12 -0600

    Update build_node_package.ts
    `--allow-env+=XDG_CACHE_HOME`

--- scripts/build_node_package.ts
+++ scripts/build_node_package.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S deno run --allow-env=DENO_AUTH_TOKENS,DENO_DIR,HOME --allow-read --allow-write --allow-run=npm
+#!/usr/bin/env -S deno run --allow-env=DENO_AUTH_TOKENS,DENO_DIR,HOME,XDG_CACHE_HOME --allow-read --allow-write --allow-run=npm
 
 import { build, BuildOptions } from "https://deno.land/x/dnt@0.7.4/mod.ts";
 
