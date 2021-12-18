a30000a000ebf66ab7861f372b59d368f0edfbcb
Mark Fulton <mfulton26@gmail.com>
2021-12-18 07:26:34 -0600

    feat: add helpful non-global prototypes

---
+++ src/prototypes/AsyncIterator.ts
@@ -0,0 +1,5 @@
+import AsyncGeneratorFunction from "../constructors/AsyncGeneratorFunction.ts";
+
+export default Object.getPrototypeOf(
+  AsyncGeneratorFunction.prototype.prototype
+);
---
+++ src/prototypes/Iterator.ts
@@ -0,0 +1,3 @@
+import GeneratorFunction from "../constructors/GeneratorFunction.ts";
+
+export default Object.getPrototypeOf(GeneratorFunction.prototype.prototype);
